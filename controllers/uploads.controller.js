cd997d76e47efa93fb55fa98d5b855edce539f43
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-21 01:14:24 -0500

    Add helper uploadFile

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,20 +1,15 @@
 import { request, response } from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { uploadFileHelper } from "../helpers/uploadFile.js";
 
 export const uploadFile = async (req = request, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-    res.status(400).send("No files were uploaded.");
-    return;
+    return res.status(400).send("No files were uploaded.");
   }
 
-  const { file } = req.files;
-  console.log(__dirname);
-  const uploadPath = __dirname + "/../uploads/" + file.name;
-
-  file.mv(uploadPath, (err) => {
-    if (err) return res.status(500).send({ err });
-    res.json({ msg: "File uploaded to " + uploadPath });
-  });
+  try {
+    const nameFile = await uploadFileHelper(req.files);
+    res.json({ nameFile });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
---
+++ helpers/uploadFile.js
@@ -0,0 +1,31 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export const uploadFileHelper = (
+  files,
+  validExtensions = ["png", "jpg", "jpeg", "gif"],
+  folder = ""
+) => {
+  return new Promise((resolve, reject) => {
+    const { file } = files;
+    const cutName = file.name.split(".");
+    const fileExtension = cutName[cutName.length - 1];
+
+    //Validar la extension
+    if (!validExtensions.includes(fileExtension)) {
+      return reject(
+        `La extension ${fileExtension} no es permitida, ${validExtensions}`
+      );
+    }
+
+    const finalName = uuidv4() + "." + fileExtension;
+    const uploadPath = __dirname + "/../uploads/" + folder + finalName;
+
+    file.mv(uploadPath, (err) => {
+      if (err) return reject(err);
+      resolve(finalName);
+    });
+  });
+};
